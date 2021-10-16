03cc644b3e747b2df1f1a30a37bf66a74b6c6e4c
etienne robaglia <etienne.robaglia@ext.mpsa.com>
2021-10-16 22:50:28 +0700

    cLaoBasemap: change town/city marker

--- src/map/LaoBasemap.vue
+++ src/map/LaoBasemap.vue
@@ -3,53 +3,40 @@
     layer-type="overlay"
     name="Lao Administrative"
   >
-    <l-circle-marker
+
+
+    <l-marker
       v-for="feature in XhkHpnMainTowns.features"
       :visible="displayXhkHpnInfo"
       :key="feature.properties['name:en']"
       :lat-lng="[feature.geometry.coordinates[1], feature.geometry.coordinates[0]]"
-      :radius="setRadiusTowns"
-      :opacity="1"
-      :fillOpacity="1"
-      :color="$mapStyleBlack"
-      :fillColor="$mapStyleBlack"
-      :options="{
-        interactive: false,
-      }"
+      :options="{interactive: false}"
     >
-      <l-tooltip
-        :options="{
-          permanent: true,
-          direction: 'bottom',
-          className: 'tooltip-town'
-        }"
-      >
-      {{feature.properties['name:en']}}
-      </l-tooltip>
-    </l-circle-marker>
-    <l-circle-marker
+      <l-icon>
+        <div class="dot-marker">
+          <svg class="svg-icon" viewBox="0 0 20 20">
+              <path fill="black" d="M11.469,10l7.08-7.08c0.406-0.406,0.406-1.064,0-1.469c-0.406-0.406-1.063-0.406-1.469,0L10,8.53l-7.081-7.08 c-0.406-0.406-1.064-0.406-1.469,0c-0.406,0.406-0.406,1.063,0,1.469L8.531,10L1.45,17.081c-0.406,0.406-0.406,1.064,0,1.469 c0.203,0.203,0.469,0.304,0.735,0.304c0.266,0,0.531-0.101,0.735-0.304L10,11.469l7.08,7.081c0.203,0.203,0.469,0.304,0.735,0.304 c0.267,0,0.532-0.101,0.735-0.304c0.406-0.406,0.406-1.064,0-1.469L11.469,10z"></path>
+          </svg>
+          <div>{{feature.properties['name:en']}}</div>
+      </div>
+      </l-icon>
+    </l-marker>
+
+    <l-marker
       v-for="feature in XhkHpnVteCities.features"
       :key="feature.properties['name:en']"
       :lat-lng="[feature.geometry.coordinates[1], feature.geometry.coordinates[0]]"
-      :radius="setRadiusCities"
-      :opacity="1"
-      :fillOpacity="1"
-      :color="$mapStyleBlack"
-      :fillColor="$mapStyleBlack"
-      :options="{
-        interactive: false,
-      }"
+      :options="{interactive: false}"
     >
-      <l-tooltip
-        :options="{
-          permanent: true,
-          direction: 'auto',
-          className: 'tooltip-city'
-        }"
-      >
-      {{feature.properties['name:en']}}
-      </l-tooltip>
-    </l-circle-marker>
+      <l-icon>
+        <div class="dot-marker">
+          <svg class="svg-icon" viewBox="0 0 20 20">
+              <path fill="black" d="M11.469,10l7.08-7.08c0.406-0.406,0.406-1.064,0-1.469c-0.406-0.406-1.063-0.406-1.469,0L10,8.53l-7.081-7.08 c-0.406-0.406-1.064-0.406-1.469,0c-0.406,0.406-0.406,1.063,0,1.469L8.531,10L1.45,17.081c-0.406,0.406-0.406,1.064,0,1.469 c0.203,0.203,0.469,0.304,0.735,0.304c0.266,0,0.531-0.101,0.735-0.304L10,11.469l7.08,7.081c0.203,0.203,0.469,0.304,0.735,0.304 c0.267,0,0.532-0.101,0.735-0.304c0.406-0.406,0.406-1.064,0-1.469L11.469,10z"></path>
+          </svg>
+          <div>{{feature.properties['name:en']}}</div>
+      </div>
+      </l-icon>
+    </l-marker>
     
     <l-geo-json
       :name="'XhkHpnPrimaryRoads'"
@@ -105,7 +92,11 @@
 </template>
 
 <script>
-import { LLayerGroup, LGeoJson, LCircleMarker, LTooltip } from 'vue2-leaflet';
+import { 
+  LLayerGroup,
+  LGeoJson,
+  LMarker,
+  LIcon } from 'vue2-leaflet';
 import L from 'leaflet';
 
 import XhkHpnPrimaryRoads from '../assets/xhk_hpn-primary_roads';
@@ -121,8 +112,8 @@ export default {
   components: {
     LLayerGroup,
     LGeoJson,
-    LCircleMarker,
-    LTooltip
+    LMarker,
+    LIcon
   },
   props: {
     filters: Object,
