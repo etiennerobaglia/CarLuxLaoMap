96c37078521e8112aebee4ffe973f178c6f4a90e
etienne robaglia <etienne.robaglia@ext.mpsa.com>
2021-10-17 21:29:38 +0700

    cLaoBaseMap: towns name > l-tooltip

--- src/map/LaoBasemap.vue
+++ src/map/LaoBasemap.vue
@@ -3,25 +3,28 @@
     layer-type="overlay"
     name="Lao Administrative"
   >
-
-
     <l-marker
       v-for="feature in XhkHpnMainTowns.features"
       :visible="displayXhkHpnInfo"
       :key="feature.properties['name:en']"
       :lat-lng="[feature.geometry.coordinates[1], feature.geometry.coordinates[0]]"
-      :options="{interactive: false}"
+      :options="{interactive: true}"
     >
       <l-icon>
         <div class="dot-marker">
-          <svg class="svg-icon" viewBox="0 0 20 20">
-              <path fill="black" d="M11.469,10l7.08-7.08c0.406-0.406,0.406-1.064,0-1.469c-0.406-0.406-1.063-0.406-1.469,0L10,8.53l-7.081-7.08 c-0.406-0.406-1.064-0.406-1.469,0c-0.406,0.406-0.406,1.063,0,1.469L8.531,10L1.45,17.081c-0.406,0.406-0.406,1.064,0,1.469 c0.203,0.203,0.469,0.304,0.735,0.304c0.266,0,0.531-0.101,0.735-0.304L10,11.469l7.08,7.081c0.203,0.203,0.469,0.304,0.735,0.304 c0.267,0,0.532-0.101,0.735-0.304c0.406-0.406,0.406-1.064,0-1.469L11.469,10z"></path>
-          </svg>
-          <div>{{feature.properties['name:en']}}</div>
-      </div>
+          ◾️
+        </div>
       </l-icon>
+      <l-tooltip
+        :options="{
+          permanent: false,
+          direction: 'auto',
+          interactive: false,
+        }"
+      >
+        {{feature.properties['name:en']}}
+      </l-tooltip>
     </l-marker>
-
     <l-marker
       v-for="feature in XhkHpnVteCities.features"
       :key="feature.properties['name:en']"
@@ -33,11 +36,19 @@
           <svg class="svg-icon" viewBox="0 0 20 20">
               <path fill="black" d="M11.469,10l7.08-7.08c0.406-0.406,0.406-1.064,0-1.469c-0.406-0.406-1.063-0.406-1.469,0L10,8.53l-7.081-7.08 c-0.406-0.406-1.064-0.406-1.469,0c-0.406,0.406-0.406,1.063,0,1.469L8.531,10L1.45,17.081c-0.406,0.406-0.406,1.064,0,1.469 c0.203,0.203,0.469,0.304,0.735,0.304c0.266,0,0.531-0.101,0.735-0.304L10,11.469l7.08,7.081c0.203,0.203,0.469,0.304,0.735,0.304 c0.267,0,0.532-0.101,0.735-0.304c0.406-0.406,0.406-1.064,0-1.469L11.469,10z"></path>
           </svg>
-          <div>{{feature.properties['name:en']}}</div>
       </div>
       </l-icon>
+      <l-tooltip
+        :options="{
+          permanent: true,
+          direction: 'auto',
+          interactive: false,
+        }"
+      >
+        {{feature.properties['name:en']}}
+      </l-tooltip>
     </l-marker>
-    
+
     <l-geo-json
       :name="'XhkHpnPrimaryRoads'"
       :geojson="XhkHpnPrimaryRoads"
@@ -96,7 +107,9 @@ import {
   LLayerGroup,
   LGeoJson,
   LMarker,
-  LIcon } from 'vue2-leaflet';
+  LIcon,
+  LTooltip,
+} from 'vue2-leaflet';
 import L from 'leaflet';
 
 import XhkHpnPrimaryRoads from '../assets/xhk_hpn-primary_roads';
@@ -113,7 +126,8 @@ export default {
     LLayerGroup,
     LGeoJson,
     LMarker,
-    LIcon
+    LIcon,
+    LTooltip,
   },
   props: {
     filters: Object,
